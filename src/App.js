2a560dc80db53159dd4e4d350ccf2a80ef7c817f
Andreas Kiener <andreas.kiener@gmail.com>
2022-07-29 15:51:49 +0200

    Add fast-fuzzy for search

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
       <Header />
       <ShoppingList />
       <Form />
-      <ShowProduce />
     </ContentArea>
   );
 }
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useStore from "./useStore";
 
 export default function Form() {
   const [formInput, setFormInput] = useState();
@@ -6,13 +7,27 @@ export default function Form() {
   function handleChange(event) {
     setFormInput(event.target.value);
   }
+  const getProduce = useStore((state) => state.getProduce);
+  getProduce();
+  const items = useStore((state) => state.items);
   return (
     <>
       <h2>Was willst du einkaufen?</h2>
       <form>
-        <input type={"text"} onChange={handleChange}></input>
+        <input
+          type={"text"}
+          name={"whatToBuy"}
+          id={"what-to-bux"}
+          placeholder={"Tippe um zu suchen…"}
+          onChange={handleChange}
+        ></input>
         <p>{formInput}</p>
       </form>
+      <ul>
+        {items.map((item) => (
+          <li>{item.name.de}</li>
+        ))}
+      </ul>
     </>
   );
 }
